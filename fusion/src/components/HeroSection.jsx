fb7ca8922caa42051020ea460497c074d53d9b27
Kenan Cosic <45003119+Nokee221@users.noreply.github.com>
2024-02-14 00:52:54 +0100

    blur on backgroound img

--- fusion/src/components/HeroSection.jsx
+++ fusion/src/components/HeroSection.jsx
@@ -26,7 +26,19 @@ const Hero = () => {
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="container mx-auto flex justify-around h-full">
+      {/* Div za pozadinu sa zamućenjem */}
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1592398289517-d9437d109bc2?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          filter: "blur(10px)" // Dodajemo zamućenje samo na pozadinsku sliku
+        }}
+      ></div>
+
+      <div className="container mx-auto flex justify-around h-full relative">
         <div className="flex flex-col justify-center items-center text-center">
           <h1
             className={`uppercase text-6xl leading-[1.1] font-semibold mb-8 ${
