ba28b747ef1548954a61ea8f0b64c9ccd1be86a1
burosalih <117530059+burosalih@users.noreply.github.com>
2024-02-16 00:57:20 +0100

    Popravke css-a i dodavanje cijene

--- fusion/src/pages/ProductDetails.jsx
+++ fusion/src/pages/ProductDetails.jsx
@@ -21,11 +21,11 @@ const ProductDetails = () => {
 
   const { naziv, cijena, opis, slika } = product;
   return (
-    <section className="pt-[450px] md:pt-32 pb-[400px] md:pb-12 lg:py-32 h-screen flex items-center">
+    <section className="pt-[150px] md:pt-32 pb-[100px] md:pb-32 lg:py-32 h-full flex items-center">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center">
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
-            <img className="max-w-[200px] lg:max-w-xs" src={slika} alt="" />
+            <img className="max-w-[200px] lg:max-w-full" src={slika} alt="" />
           </div>
           <div className="flex-1 text-center lg:text-left">
             <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">
@@ -38,7 +38,7 @@ const ProductDetails = () => {
             {parse(opis)}
             <button
               onClick={() => addToCart(product, product._id)}
-              className="bg-primary py-4 px-8 text-white"
+              className="bg-primary py-4 mt-4 px-8 text-white"
             >
               Dodaj u korpu
             </button>
