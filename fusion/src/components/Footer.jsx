19eb027db37cb251865dfb144d14a9da2b59f8f4
burosalih <117530059+burosalih@users.noreply.github.com>
2024-03-02 15:54:32 +0100

    popravljen responsive na footeru

--- fusion/src/components/Footer.jsx
+++ fusion/src/components/Footer.jsx
@@ -7,52 +7,51 @@ const Footer = forwardRef((props, ref) => (
     id="footer"
     className="bg-gradient-to-b from-primary to-emerald-700 py-12"
   >
-    <div className="container mx-auto flex items-center justify-between">
-      <div className="text-white">
+    <div className="container mx-auto flex flex-col md:flex-row md:items-center justify-between">
+      <div className="text-white mb-8 md:mr-8">
         <p>Kontaktirajte nas: </p>
-        <a href="tel:+387644094444" className="flex items-center py-4 text-xl">
-          <FaPhone className="mr-2 text-2xl" />
+        <a href="tel:+387644094444" className="flex items-center py-2 text-md md:text-xl">
+          <FaPhone className="mr-2 text-xl md:text-2xl" />
           <p>+387644094444</p>
         </a>
-        <div className="flex items-center py-4 text-xl">
-          <FaEnvelope className="mr-2 text-2xl" />
+        <div className="flex items-center py-2 text-md md:text-xl">
+          <FaEnvelope className="mr-2 text-xl md:text-2xl" />
           <p>
             <a href="mailto:kamagrabalk@gmail.com">kamagrabalk@gmail.com</a>
           </p>
         </div>
       </div>
-      <div className="text-white">
-        <p>Partneri stanice: </p>
+      <div className="text-white mb-8 md:mb-0 md:mr-8">
+        <p className="lg:text-center">Partneri stanice: </p>
         <a
           href="https://kamagra-hrvatska.com/"
-          target="_blank" // Ovo otvara link u novom tabu
-          rel="noopener noreferrer" // Preporučeno zbog sigurnosti
-          className="flex items-center py-4 text-xl"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center py-2 text-lg md:text-xl"
         >
-          <FaGlobe className="mr-2 text-2xl" />
-          <p>KAMAGRA BALKAN</p>
+          <FaGlobe className="mr-2 text-xl md:text-2xl" />
+          <p className="md:text-sm lg:text-lg">KAMAGRA BALKAN</p>
         </a>
         <a
           href="https://kamagra-hrvatska.com/"
-          target="_blank" // Ovo otvara link u novom tabu
-          rel="noopener noreferrer" // Preporučeno zbog sigurnosti
-          className="flex items-center py-4 text-xl"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center py-2 text-lg md:text-xl"
         >
-          <FaGlobe className="mr-2 text-2xl" />
-          <p>KAMAGRA BOSNA</p>
+          <FaGlobe className="mr-2 text-xl md:text-2xl" />
+          <p className="md:text-sm lg:text-lg">KAMAGRA BOSNA</p>
         </a>
         <a
           href="https://kamagra-hrvatska.com/"
-          target="_blank" // Ovo otvara link u novom tabu
-          rel="noopener noreferrer" // Preporučeno zbog sigurnosti
-          className="flex items-center py-4 text-xl"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center py-2 text-lg md:text-xl"
         >
-          <FaGlobe className="mr-2 text-2xl" />
-          <p>KAMAGRA BALKAN</p>
+          <FaGlobe className="mr-2 text-xl md:text-2xl" />
+          <p className="md:text-sm lg:text-lg">KAMAGRA BALKAN</p>
         </a>
       </div>
-
-      <div className="text-white text-right">© 2024 KAMAGRA BALKAN</div>
+      <div className="text-white md:text-center">© 2024 KAMAGRA BALKAN</div>
     </div>
   </footer>
 ));
